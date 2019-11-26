5a14075d5b380438b3ca8243f9edc2040314abb4
rajeevramdas <rajeevramdas@aol.com>
2019-11-26 15:30:31 -0500

    Third commit

---
+++ cf/slid/runcf.sh
@@ -0,0 +1,4 @@
+aws cloudformation deploy --template-file /Users/thottr/Docs/aws/cloudform/templates/datalake/sldlid/kdg.yaml \
+--parameter-overrides yourEmail=<your-email-here> \
+--stack-name kdgStack --capabilities CAPABILITY_NAMED_IAM
+
