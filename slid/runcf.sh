94363cd744ba39660c6bbaa2c853c6348412fd46
rajeevramdas <rajeevramdas@aol.com>
2019-11-26 22:08:42 -0500

    Commit

--- slid/runcf.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation deploy --template-file /Users/thottr/Docs/aws/cloudform/templates/datalake/sldlid/kdg.yaml \
---parameter-overrides yourEmail=<Your-Email-Here> \
---stack-name kdgStack --capabilities CAPABILITY_NAMED_IAM
-
